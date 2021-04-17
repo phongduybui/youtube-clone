06602777dbcce59772c629ffd6114ea8a05a8ec7
phongduybui <buiduyphong921@gmail.com>
2021-04-17 16:23:34 +0700

    fix btn--search-mobile

--- src/reducers/setDarkModeReducer.js
+++
@@ -1,9 +0,0 @@
-import { SET_DARK_MODE } from '../actions/types';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (state = false, action) => {
-  if(action.type === SET_DARK_MODE) {
-    return !state;
-  }
-  return state;
-}
