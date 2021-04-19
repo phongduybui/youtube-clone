e717a780c912902616f78ce4815bbfefbf19d796
phongduybui <buiduyphong921@gmail.com>
2021-04-19 18:34:20 +0700

    separate main component

---
+++ src/components/Main.js
@@ -0,0 +1,27 @@
+import './Main.css';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+import ListVideos from './ListVideos';
+import SearchResults from './SearchResults';
+import LineLoader from './LineLoader';
+import WatchVideo from './WatchVideo';
+
+const Main = ({ isBarCollapse }) => {
+  const { width } = useWindowDimensions();
+  const sideBarStatus = isBarCollapse && width >= 1024 ? 'collapse' : '';
+
+  return (
+    <div className={`main ${sideBarStatus}`}>
+      <LineLoader />
+      <Route path="/" exact component={ListVideos} />
+      <Route path="/search-results" exact component={SearchResults} />
+      <Route path="/watch/:id" exact component={WatchVideo} />
+    </div>
+  )
+}
+
+const mapStateToProps = state => ({ isBarCollapse: state.isBarClick });
+
+export default connect(mapStateToProps)(Main);
