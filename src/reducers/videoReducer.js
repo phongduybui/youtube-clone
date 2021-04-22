f08a545c22604ff5d0c8f1255c613d582b0077e0
phongduybui <buiduyphong921@gmail.com>
2021-04-22 22:19:51 +0700

    refactor return statement videoReducer

--- src/reducers/videoReducer.js
+++ src/reducers/videoReducer.js
@@ -5,9 +5,11 @@ import { FETCH_HOME_VIDEOS } from '../actions/types';
 export default (state = {}, action) => {
   switch(action.type) {
     case FETCH_HOME_VIDEOS:
-      const newState = { ...state, ..._.mapKeys(action.payload.items, 'id') };
-      newState.nextPageToken = action.payload.nextPageToken;
-      return newState;
+      return { 
+        ...state, 
+        ..._.mapKeys(action.payload.items, 'id'),
+        nextPageToken: action.payload.nextPageToken
+      };
     default:
       return state;
   }
